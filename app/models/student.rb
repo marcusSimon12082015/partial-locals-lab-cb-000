5feced3e8f4345383f4249534f8fec948d9680c9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-19 15:51:50 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(name) 
+  def self.search(name)
     Student.where('name LIKE ?','%name%')
-  end 
+  end
 end
