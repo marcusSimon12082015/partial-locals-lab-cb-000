14ec63d58bbac7b99eab33befd8fba70b05aa911
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-19 16:03:47 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    #binding.pry
+    if name.blank?
+      Student.all
+    else
+      Student.where('lower(name) LIKE ?','%'+name.downcase+'%')
+    end
+  end
 end
