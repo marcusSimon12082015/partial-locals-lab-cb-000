acbd3bb9d8c4962c98b5454dbedf171252aff2dd
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-19 15:56:39 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(name)
-    binding.pry
+    #binding.pry
     if name.blank?
       Student.all
     else
